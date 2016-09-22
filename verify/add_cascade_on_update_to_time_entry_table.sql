652d6851aa7f6f3764539213bd85e33acf6a6c5a
John Andre Hestad <jah@blankoslo.no>
2016-09-22 13:31:47 +0200

    Fix time_entry cascade verify script
    It depended on data in the db. Extended script to do actual verification

--- verify/add_cascade_on_update_to_time_entry_table.sql
+++ verify/add_cascade_on_update_to_time_entry_table.sql
@@ -2,14 +2,25 @@
 
 BEGIN;
 
+INSERT INTO employees
+  (id,first_name,last_name,title, phone, email,gender,birth_date)
+  values (191915195,'Jacob','Aal','Federator', '91919191','some@email.com', 'male', '1972-01-01');
+
+INSERT INTO customers
+  (id, name)
+  values (9959931,'customersName');
+
 INSERT INTO projects
   (id, name, billable, customer)
-  values ('oldId789','rollbackName',false, 1);
+  values ('projectsId','projectsName',false, 9959931);
 
 INSERT INTO time_entry
   (employee, creator, minutes, project, date)
-  values (1, 1, 30, 'oldId789', '01-01-2016');
+  values (191915195, 191915195, 30, 'projectsId', '01-01-2099');
 
-update projects set id='newId789' where id='oldId789';
+update projects set id='newProjectsId' where id='projectsId';
+
+select 1/count(*) from projects where id='newProjectsId';
+select 1/count(*) from time_entry where project='newProjectsId';
 
 ROLLBACK;
