09a0386a9a8a0389b74ba279296c5c98e3216abb
John Andre Hestad <jah@blankoslo.no>
2016-09-22 14:52:24 +0200

    Comment out notice

--- verify/add_unique_to_name_in_customer_table.sql
+++ verify/add_unique_to_name_in_customer_table.sql
@@ -6,7 +6,7 @@ BEGIN
     insert into customers (name) values('somePossibleUnusedNamez3');
     insert into customers (name) values('somePossibleUnusedNamez3');
     EXCEPTION WHEN unique_violation THEN
-    	RAISE NOTICE 'SUCCESS: Verified that duplicate name gave us unique_violation';
+      -- SUCCESS: Verified that duplicate name gave us unique_violation
     	RETURN;
   END;
   RAISE EXCEPTION 'ERROR: Still possible to insert duplicate names.';
