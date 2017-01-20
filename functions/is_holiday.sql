5fc6f716a25a9e0837e3ee1aa18d6522e1445527
Magne Davidsen <magne.davidsen@gmail.com>
2017-01-20 16:20:39 +0100

    Keep these functions in sqitch
    Because a view is depending on them

--- functions/is_holiday.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION public.is_holiday(d date)
- RETURNS boolean
- LANGUAGE plpgsql
-AS $function$
-BEGIN
-  RETURN EXISTS (SELECT * FROM holidays WHERE holidays.date = d);
-END
-$function$
