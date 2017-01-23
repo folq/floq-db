58207af85ed8958808b2c808bce64580c1ae54ba
Magne Davidsen <magne.davidsen@gmail.com>
2017-01-23 09:56:50 +0100

    Add the simplest of deploy scripts

---
+++ functions/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+case "$1" in
+     -d|--dev)
+         HOST=floq-dev.caawuzisqucy.eu-central-1.rds.amazonaws.com
+         shift
+         ;;
+     -t|--test)
+         HOST=floq-test.caawuzisqucy.eu-central-1.rds.amazonaws.com
+         shift
+         ;;
+     -p|--prod)
+         HOST=floq.caawuzisqucy.eu-central-1.rds.amazonaws.com
+         shift
+         ;;
+     *)
+         echo "No environment specified"
+         exit 3
+         ;;
+esac
+
+for f in *.sql
+do
+ echo "deploying $f to $HOST"
+ psql -f $f -h $HOST -d floq -U root
+done
