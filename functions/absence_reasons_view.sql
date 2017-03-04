26e4e1c906098515b7be855f0f95de26aa19c1ea
Magne Davidsen <magne.davidsen@gmail.com>
2017-03-04 14:19:12 +0100

    Add hack to have avspaseringskode
    We want to be able mark avspasering in fraværskalender, but not use it for time tracking

--- functions/absence_reasons_view.sql
+++ functions/absence_reasons_view.sql
@@ -7,12 +7,13 @@ BEGIN
            OR reason = 'SYK1002'
            OR reason = 'PER1000'
            OR reason = 'PER1001'
+           OR reason = 'AVS'
          );
 END
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE VIEW absence_reasons
-         AS ( SELECT id, name
-                FROM projects
-               WHERE is_absence_reason(id)
+         AS ( SELECT id, name, billable FROM projects WHERE is_absence_reason(id)
+              UNION ALL
+              SELECT 'AVS' as id, 'Avspasering' as name, 'nonbillable' as billable;
             );
