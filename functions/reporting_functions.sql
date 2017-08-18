fa9c24ba326f80b31edb3e6612e074385c49ea77
Ole Bakstad <ole.bakstad@blank.no>
2017-08-18 11:45:56 +0200

    Make casing more consistent with how its done in other files
    Tried to make it look more like staffing_functions.sql

--- functions/reporting_functions.sql
+++ functions/reporting_functions.sql
@@ -2,18 +2,18 @@ BEGIN;
 
 
 CREATE OR REPLACE FUNCTION accumulated_staffing_hours(from_date date, to_date date)
-RETURNS TABLE (available_hours NUMERIC, billable_hours NUMERIC, nonbillable_hours NUMERIC, unavailable_hours NUMERIC) AS
+RETURNS TABLE (available_hours numeric, billable_hours numeric, nonbillable_hours numeric, unavailable_hours numeric) AS
 $$
 BEGIN
   RETURN QUERY (
 SELECT
-    (select count(work_day) * 7.5 as avail_hours FROM possible_work_dates_per_employee(from_date, to_date)) AS available_hours,
-    sum(case when staff.billable = 'billable' :: TIME_STATUS then 1 else 0 end) * 7.5 AS billable_hours,
-    sum(case when staff.billable = 'nonbillable' :: TIME_STATUS then 1 else 0 end) * 7.5 AS nonbillable_hours,
-    sum(case when staff.billable = 'unavailable' :: TIME_STATUS then 1 else 0 end) * 7.5 AS unavailable_hours
-  FROM (select * FROM staffing
+    (SELECT COUNT(work_day) * 7.5 as avail_hours FROM possible_work_dates_per_employee(from_date, to_date)) AS available_hours,
+    SUM(CASE WHEN staff.billable = 'billable' :: time_status THEN 1 ELSE 0 END) * 7.5 AS billable_hours,
+    SUM(CASE WHEN staff.billable = 'nonbillable' :: time_status THEN 1 ELSE 0 END) * 7.5 AS nonbillable_hours,
+    SUM(CASE WHEN staff.billable = 'unavailable' :: time_status THEN 1 ELSE 0 END) * 7.5 AS unavailable_hours
+  FROM (SELECT * FROM staffing
     JOIN projects ON (staffing.project = projects.id)
-    WHERE staffing.date BETWEEN from_date AND to_date) as staff
+    WHERE staffing.date BETWEEN from_date AND to_date) AS staff
 
   );
 END
@@ -25,7 +25,7 @@ RETURNS TABLE (employee_id integer, work_day date) AS
 $$
 BEGIN
   RETURN QUERY (
-    select
+    SELECT
       x.emp_id,
       x.work_day
     FROM
@@ -36,7 +36,7 @@ BEGIN
          generate_series :: DATE AS work_day
        FROM employees e
          CROSS JOIN
-           generate_series(from_date, to_date, '1 day' :: INTERVAL)
+           generate_series(from_date, to_date, '1 day' :: interval)
       ) AS x
     WHERE
       is_possible_work_day(x.emp_id, x.work_day)
@@ -45,33 +45,33 @@ END
 $$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION is_possible_work_day(in_employee_id NUMERIC, in_date DATE)
+CREATE OR REPLACE FUNCTION is_possible_work_day(in_employee_id numeric, in_date date)
   RETURNS BOOLEAN AS
 $$
 BEGIN
   RETURN (
     is_weekday(in_date)
     AND NOT is_holiday(in_date)
-    AND NOT exists(SELECT *
+    AND NOT EXISTS(SELECT *
                    FROM employees e
                    WHERE e.id = in_employee_id AND e.termination_date < in_date)
-    AND exists(SELECT *
+    AND EXISTS(SELECT *
                FROM employees e
                WHERE e.id = in_employee_id AND e.date_of_employment <= in_date)
     AND (
-      NOT exists(SELECT *
+      NOT EXISTS(SELECT *
                  FROM staffing s
                  WHERE s.employee = in_employee_id AND s.date = in_date)
-      OR exists(SELECT *
+      OR EXISTS(SELECT *
                 FROM staffing s
                   JOIN projects p ON s.project = p.id
-                WHERE s.employee = in_employee_id AND s.date = in_date AND p.billable != 'unavailable' :: TIME_STATUS)
+                WHERE s.employee = in_employee_id AND s.date = in_date AND p.billable != 'unavailable' :: time_status)
     )
-    AND NOT exists(SELECT *
+    AND NOT EXISTS(SELECT *
                    FROM absence a
                      JOIN absence_reasons ar ON (ar.id = a.reason)
                    WHERE
-                     a.employee_id = in_employee_id AND a.date = in_date AND ar.billable = 'unavailable' :: TIME_STATUS)
+                     a.employee_id = in_employee_id AND a.date = in_date AND ar.billable = 'unavailable' :: time_status)
   );
 END
 $$ LANGUAGE plpgsql;
@@ -80,39 +80,39 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION staffing_and_billing_overview(in_from_date date, in_to_date date)
 RETURNS TABLE (
-  year INT,
-  week INT,
+  year int,
+  week int,
   from_date date,
   to_date date,
-  available_hours NUMERIC,
-  billable_hours NUMERIC,
-  planned_fg NUMERIC,
-  actual_available_hours NUMERIC,
-  actual_billable_hours NUMERIC,
-  actual_fg NUMERIC,
-  deviation_available_hours NUMERIC,
-  deviation_billable_hours NUMERIC,
-  deviation_fg NUMERIC,
-  visibility NUMERIC
+  available_hours numeric,
+  billable_hours numeric,
+  planned_fg numeric,
+  actual_available_hours numeric,
+  actual_billable_hours numeric,
+  actual_fg numeric,
+  deviation_available_hours numeric,
+  deviation_billable_hours numeric,
+  deviation_fg numeric,
+  visibility numeric
 ) AS
 $$
 BEGIN
   RETURN QUERY (
 
-select
+SELECT
   x.year,
   x.week,
   x.from_date,
   x.to_date,
   planned.available_hours,
   planned.billable_hours,
-  100*(planned.billable_hours / planned.available_hours)                              as planned_fg,
-  actual.sum_available_hours                                                          as actual_available_hours,
-  actual.sum_billable_hours                                                           as actual_billable_hours,
-  100*(actual.sum_billable_hours / actual.sum_available_hours)                        as actual_fg,
-  actual.sum_available_hours - planned.available_hours                                as deviation_available_hours,
-  actual.sum_billable_hours - planned.billable_hours                                  as deviation_billable_hours,
-  100*((actual.sum_billable_hours - planned.billable_hours)/ planned.available_hours) as deviation_fg,
+  100*(planned.billable_hours / planned.available_hours)                              AS planned_fg,
+  actual.sum_available_hours                                                          AS actual_available_hours,
+  actual.sum_billable_hours                                                           AS actual_billable_hours,
+  100*(actual.sum_billable_hours / actual.sum_available_hours)                        AS actual_fg,
+  actual.sum_available_hours - planned.available_hours                                AS deviation_available_hours,
+  actual.sum_billable_hours - planned.billable_hours                                  AS deviation_billable_hours,
+  100*((actual.sum_billable_hours - planned.billable_hours)/ planned.available_hours) AS deviation_fg,
   v.visibility
 FROM
   (SELECT
@@ -126,10 +126,10 @@ FROM
    FROM
      (
        SELECT
-         EXTRACT(YEAR FROM dd) :: INTEGER AS year,
-         EXTRACT(WEEK FROM dd) :: INTEGER AS week
+         EXTRACT(YEAR FROM dd) :: integer AS year,
+         EXTRACT(WEEK FROM dd) :: integer AS week
        FROM
-         generate_series(in_from_date, in_to_date, '1 week' :: INTERVAL) dd
+         generate_series(in_from_date, in_to_date, '1 week' :: interval) dd
      ) tt
   ) x
   LEFT OUTER JOIN reporting_visibility v ON (v.year = x.year AND v.week = x.week),
@@ -142,14 +142,14 @@ $$ LANGUAGE plpgsql;
 
 
 CREATE OR REPLACE FUNCTION accumulated_billed_hours(from_date date, to_date date)
-RETURNS TABLE (sum_available_hours NUMERIC, sum_billable_hours NUMERIC) AS
+RETURNS TABLE (sum_available_hours numeric, sum_billable_hours numeric) AS
 $$
 BEGIN
   RETURN QUERY (
     SELECT
-         sum(available_hours) :: NUMERIC AS sum_available_hours,
-         sum(billable_hours)  :: NUMERIC AS sum_billable_hours
-       FROM time_tracking_status (from_date, to_date)
+         SUM(available_hours) :: numeric AS sum_available_hours,
+         SUM(billable_hours)  :: numeric AS sum_billable_hours
+       FROM time_tracking_status(from_date, to_date)
       );
 END
 $$ LANGUAGE plpgsql;
