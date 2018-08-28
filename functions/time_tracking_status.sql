9265353d8f955ffba57de6e10b674096c25d704a
Kristiane Westgård <kristiane.westgard@blank.no>
2018-08-28 11:24:48 +0200

    Split main timetracker function in smaller pieces, and added new function for getting FG

--- functions/time_tracking_status.sql
+++ functions/time_tracking_status.sql
@@ -1,3 +1,58 @@
+CREATE OR REPLACE FUNCTION public.unavailable_hours_for_employees(start_date date, end_date date)
+ RETURNS TABLE(id integer, sum bigint)
+ LANGUAGE plpgsql
+ STABLE STRICT
+AS $function$
+begin
+  return query (
+    -- find sum of unavailable time (holidays, vacation days, sick leave, etc.) per employee
+  select t.employee, sum(minutes)
+  from time_entry t, projects p
+  where t.project = p.id
+  and t.date between start_date and end_date
+  and p.billable = 'unavailable'
+  group by t.employee
+  );
+end
+$function$;
+
+CREATE OR REPLACE FUNCTION public.billable_hours_for_employees(start_date date, end_date date)
+ RETURNS TABLE(id integer, sum bigint)
+ LANGUAGE plpgsql
+ STABLE STRICT
+AS $function$
+begin
+  return query (
+    -- find sum of unavailable time (holidays, vacation days, sick leave, etc.) per employee
+  select t.employee, sum(minutes)
+  from time_entry t, projects p
+  where t.project = p.id
+  and t.date between start_date and end_date
+  and p.billable = 'billable'
+  group by t.employee
+  );
+end
+$function$;
+
+CREATE OR REPLACE FUNCTION public.nonbillable_hours_for_employees(start_date date, end_date date)
+ RETURNS TABLE(id integer, sum bigint)
+ LANGUAGE plpgsql
+ STABLE STRICT
+AS $function$
+begin
+  return query (
+    -- find sum of nonbillable hours worked per employee
+    select t.employee, sum(minutes)
+    from time_entry t,
+         projects p
+    where t.project = p.id
+      and t.date between start_date and end_date
+      and p.billable = 'nonbillable'
+    group by t.employee
+  );
+end
+$function$;
+
 CREATE OR REPLACE FUNCTION public.time_tracking_status(start_date date, end_date date)
  RETURNS TABLE(name text, email text, available_hours double precision, billable_hours double precision, non_billable_hours double precision, unavailable_hours double precision, unregistered_days integer, last_date date, last_created date)
  LANGUAGE plpgsql
@@ -5,68 +60,85 @@ CREATE OR REPLACE FUNCTION public.time_tracking_status(start_date date, end_date
 AS $function$
 begin
   return query (
-    select e.first_name || ' ' || e.last_name,
-           e.email,
-           business_hours(greatest(e.date_of_employment, start_date), least(e.termination_date, end_date)) - coalesce(sum(t.unavailable_hours)/60.0, 0.0)::float8,
-           coalesce(sum(t.billable_hours)/60.0, 0.0)::float8,
-           coalesce(sum(t.non_billable_hours)/60.0, 0.0)::float8,
-           coalesce(sum(t.unavailable_hours)/60.0, 0.0)::float8,
-           (select * from unregistered_days(start_date,end_date,e.id) u),
-           ld.date,
-           lc.created::date
+    select e.first_name || ' ' || e.last_name as name,
+       e.email,
+       business_hours(greatest(e.date_of_employment, start_date), least(e.termination_date, end_date)) - coalesce(sum(t.unavailable_hours)/60.0, 0.0)::float8 as available_hours,
+       coalesce(sum(t.billable_hours)/60.0, 0.0)::float8 as billable_hours,
+       coalesce(sum(t.non_billable_hours)/60.0, 0.0)::float8 as non_billable_hours,
+       coalesce(sum(t.unavailable_hours)/60.0, 0.0)::float8 as unavailable_hours,
+       (select * from unregistered_days(start_date,end_date,e.id) u) as unregistered_days,
+       ld.date,
+       lc.created::date
     from employees e
-    --(select unregistered_days from unregistered_days(start_date,end_date,e.id)) as unregistered_days
     left join (
-       select coalesce(uah.employee, bh.employee,nbh.employee) as employee,
-               uah.sum as unavailable_hours,
-               bh.sum as billable_hours,
-               nbh.sum as non_billable_hours
+        select coalesce(uah.id, bh.id, nbh.id) as employee_id,
+            uah.sum as unavailable_hours,
+            bh.sum as billable_hours,
+            nbh.sum as non_billable_hours
         from (
-            -- find sum of unavailable time (holidays, vacation days, sick leave, etc.) per employee
-            select t.employee, sum(minutes)
-            from time_entry t,
-                 projects p
-            where t.project = p.id
-              and t.date between start_date and end_date
-              and p.billable = 'unavailable'
-            group by t.employee
+          select * from unavailable_hours_for_employees(start_date,end_date)
         ) uah
         full outer join (
-            -- find sum of billable hours worked per employee
-            select t.employee, sum(minutes)
-            from time_entry t,
-                 projects p
-            where t.project = p.id
-              and t.date between start_date and end_date
-              and p.billable = 'billable'
-            group by t.employee
-        ) bh
-        on uah.employee = bh.employee
-	full outer join (
-            -- find sum of billable hours worked per employee
-            select t.employee, sum(minutes)
-            from time_entry t,
-                 projects p
-            where t.project = p.id
-              and t.date between start_date and end_date
-              and p.billable = 'nonbillable'
-            group by t.employee
-        ) nbh
-        on uah.employee = nbh.employee) as t on t.employee = e.id
+          select * from billable_hours_for_employees(start_date,end_date)
+        ) bh on uah.id = bh.id
+    	  full outer join (
+          select * from billable_hours_for_employees(start_date,end_date)
+        ) nbh on uah.id = nbh.id
+    ) as t on t.employee_id = e.id
+
     -- find last time_entry date for employees
     left join (
       select distinct on(te.employee) te.employee, te.date
-        from time_entry te order by te.employee, te.date desc
+          from time_entry te 
+          order by te.employee, te.date desc
     ) ld on ld.employee=e.id
+
     -- find last time_entry created for employees
     left join (
       select distinct on(te.employee) te.employee, te.created
-        from time_entry te order by te.employee, te.created desc
+          from time_entry te 
+          order by te.employee, te.created desc
     ) lc on lc.employee=e.id
+
     where e.date_of_employment <= end_date
-      and (e.termination_date is null or e.termination_date >= start_date)
+    and (e.termination_date is null or e.termination_date >= start_date)
     group by e.id, ld.date, lc.created
     order by e.first_name, e.last_name
+  );
+end
+$function$
+
+CREATE OR REPLACE FUNCTION public.fg_for_employee(emp_id integer, start_date date, end_date date)
+ RETURNS TABLE(available_hours double precision, billable_hours double precision)
+ LANGUAGE plpgsql
+ STABLE STRICT
+AS $function$
+begin
+  return query (
+	select 
+		business_hours(greatest(e.date_of_employment, start_date), least(e.termination_date, end_date)) - coalesce(sum(employee.unavailable_hours)/60.0, 0.0)::float8 as available_hours,
+	    coalesce(sum(employee.billable_hours)/60.0, 0.0)::float8 as billable_hours
+	from employees e
+	left join (
+	
+	   select coalesce(uah.id, bh.id, nbh.id) as employee_id,
+	   		uah.sum as unavailable_hours,
+	        bh.sum as billable_hours,
+	        nbh.sum as non_billable_hours
+	    from (
+	        select * from unavailable_hours_for_employees(start_date,end_date)
+	    ) uah
+	    full outer join (
+	    	select * from billable_hours_for_employees(start_date,end_date)
+	    ) bh on uah.id = bh.id
+	  	full outer join (
+	      select * from billable_hours_for_employees(start_date,end_date)
+	    ) nbh on uah.id = nbh.id
+	    
+	) as employee on employee.employee_id = e.id
+	
+	where e.id = emp_id
+	group by e.id
   );
 end
 $function$
