4a1644f97286bff8110152bdc2fc9fcf5b202ab0
Michael Eikeland <michael.eikeland@blank.no>
2019-10-15 14:15:24 +0200

    add function to lock employee hours - given project code, start, end and commit date

---
+++ functions/lock_employee_hours.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION public.lock_employee_hours(in_project text, in_start date, in_end date, in_commit date, in_creator integer)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+  BEGIN
+    INSERT INTO
+        timelock (employee, creator, commit_date)
+    SELECT DISTINCT
+        te.employee, te.employee, in_commit
+    FROM time_entry AS te
+    WHERE te.project = in_project AND te.date >= in_start AND te.date <= in_end;
+  END;
+$function$
