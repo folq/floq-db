3cfc0fc1d51d323b36bc5d9dab8090b24814ef67
Trond Øydna <trond.oydna@blank.no>
2019-11-06 14:30:17 +0100

    employees.title is a required field i Folq's Floq DB istance

--- sqitch/verify/employee_role.sql
+++ sqitch/verify/employee_role.sql
@@ -2,8 +2,8 @@
 
 BEGIN;
 
-INSERT INTO employees(id, first_name, last_name, phone, email, gender, birth_date)
-VALUES (-77, 'Admin', 'Istrator', '11223344', 'admin@blank.no', 'male', '1970-01-01');
+INSERT INTO employees(id, first_name, last_name, phone, email, gender, birth_date, title)
+VALUES (-77, 'Admin', 'Istrator', '11223344', 'admin@blank.no', 'male', '1970-01-01', 'title');
 
 INSERT INTO employee_role(employee_id, role_type)
 VALUES (-77, 'admin');
