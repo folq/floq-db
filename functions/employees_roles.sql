2ef7e0b3e78ea7b06728571707410cd9cf6bcf21
Trond Øydna <trond.oydna@blank.no>
2019-11-06 16:40:21 +0100

    simplified employees_roles() by calling employees_roles(id)

--- functions/employees_roles.sql
+++ functions/employees_roles.sql
@@ -3,10 +3,8 @@ CREATE OR REPLACE FUNCTION public.employees_roles()
     LANGUAGE sql
     STABLE STRICT
 AS $function$
-SELECT employees.id, first_name, last_name, email, array_remove(array_agg(role_type), NULL) AS roles
-FROM employees LEFT OUTER JOIN employee_role ON (employees.id = employee_role.employee_id)
-GROUP BY employees.id
-ORDER BY employees.id
+    SELECT employees_roles(employees.id)
+    FROM employees
 $function$;
 
 CREATE OR REPLACE FUNCTION public.employees_roles(id_param INTEGER)
@@ -14,9 +12,9 @@ CREATE OR REPLACE FUNCTION public.employees_roles(id_param INTEGER)
     LANGUAGE sql
     STABLE STRICT
 AS $function$
-SELECT employees.id, first_name, last_name, email, array_remove(array_agg(role_type), NULL) AS roles
-FROM employees LEFT OUTER JOIN employee_role ON (employees.id = employee_role.employee_id)
-WHERE employees.id = id_param
-GROUP BY employees.id
-ORDER BY employees.id
+    SELECT employees.id, first_name, last_name, email, array_remove(array_agg(role_type), NULL) AS roles
+    FROM employees LEFT OUTER JOIN employee_role ON (employees.id = employee_role.employee_id)
+    WHERE employees.id = id_param
+    GROUP BY employees.id
+    ORDER BY employees.id
 $function$;
