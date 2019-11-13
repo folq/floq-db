be6cba6c78208551aa0475e310913699b067f6f4
Trond Øydna <trond.oydna@blank.no>
2019-11-13 15:44:08 +0100

    added aother employees_roles function, this one takes email

--- functions/employees_roles.sql
+++ functions/employees_roles.sql
@@ -1,20 +1,31 @@
 CREATE OR REPLACE FUNCTION public.employees_roles(id_param INTEGER)
-    RETURNS TABLE(id INTEGER, first_name TEXT, last_name TEXT, email TEXT, roles employee_role_type[])
+    RETURNS TABLE(employee employees, roles employee_role_type[])
     LANGUAGE sql
     STABLE STRICT
 AS $function$
-SELECT employees.id, first_name, last_name, email, array_remove(array_agg(role_type), NULL) AS roles
-FROM employees LEFT OUTER JOIN employee_role ON (employees.id = employee_role.employee_id)
-WHERE employees.id = id_param
-GROUP BY employees.id
-ORDER BY employees.id
+    SELECT employees, array_remove(array_agg(role_type), NULL) AS roles
+    FROM employees LEFT OUTER JOIN employee_role ON (employees.id = employee_role.employee_id)
+    WHERE employees.id = id_param
+    GROUP BY employees.id
 $function$;
 
 CREATE OR REPLACE FUNCTION public.employees_roles()
-    RETURNS TABLE(id INTEGER, first_name TEXT, last_name TEXT, email TEXT, roles employee_role_type[])
+    RETURNS TABLE(employee employees, roles employee_role_type[])
     LANGUAGE sql
     STABLE STRICT
 AS $function$
     SELECT employees_roles(employees.id)
     FROM employees
+    ORDER BY employees.id
+$function$;
+
+CREATE OR REPLACE FUNCTION public.employees_roles(email_param TEXT)
+    RETURNS TABLE(employee employees, roles employee_role_type[])
+    LANGUAGE sql
+    STABLE STRICT
+AS $function$
+    SELECT employees, array_remove(array_agg(role_type), NULL) AS roles
+    FROM employees LEFT OUTER JOIN employee_role ON (employees.id = employee_role.employee_id)
+    WHERE employees.email = email_param
+    GROUP BY employees.id
 $function$;
