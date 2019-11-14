aa492a4f92573d56f5e105096e6c1866fd869bfd
Trond Øydna <trond.oydna@blank.no>
2019-11-14 15:15:22 +0100

    marked employees_roles functions as immutable

--- functions/employees_roles.sql
+++ functions/employees_roles.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE FUNCTION public.employees_roles(id_param INTEGER)
     RETURNS TABLE(employee employees, roles employee_role_type[])
-    LANGUAGE sql
-    STABLE STRICT
+    LANGUAGE SQL IMMUTABLE STRICT
 AS $function$
     SELECT employees, array_remove(array_agg(role_type), NULL) AS roles
     FROM employees LEFT OUTER JOIN employee_role ON (employees.id = employee_role.employee_id)
@@ -11,8 +10,7 @@ $function$;
 
 CREATE OR REPLACE FUNCTION public.employees_roles()
     RETURNS TABLE(employee employees, roles employee_role_type[])
-    LANGUAGE sql
-    STABLE STRICT
+    LANGUAGE SQL IMMUTABLE STRICT
 AS $function$
     SELECT employees_roles(employees.id)
     FROM employees
@@ -21,8 +19,7 @@ $function$;
 
 CREATE OR REPLACE FUNCTION public.employees_roles(email_param TEXT)
     RETURNS TABLE(employee employees, roles employee_role_type[])
-    LANGUAGE sql
-    STABLE STRICT
+    LANGUAGE SQL IMMUTABLE STRICT
 AS $function$
     SELECT employees, array_remove(array_agg(role_type), NULL) AS roles
     FROM employees LEFT OUTER JOIN employee_role ON (employees.id = employee_role.employee_id)
